7ca47b1dc2893ecdf85d3d7e99709052668f982d
Ирина Донцова <dontsova.irina@bk.ru>
2022-04-30 10:09:15 +0300

    --all

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -16,6 +16,7 @@ class Post(models.Model):
         'Group',
         blank=True, null=True,
         on_delete=models.SET_NULL, related_name='posts')
+
     class Meta:
         ordering = ['-pub_date']
 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -6,7 +6,7 @@ from .models import Post, Group
 def index(request):
     POSTS_PER_PAGE = 10
     # posts = Post.objects.order_by('-pub_date')[:10]
-    posts = Post.objects.all()
+    posts = Post.objects.all()[:POSTS_PER_PAGE]
     context = {
         'posts': posts,
     }
@@ -16,7 +16,8 @@ def index(request):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     POSTS_PER_PAGE = 10
-    # posts = Post.objects.filter(group=group).order_by('-pub_date')[:POSTS_PER_PAGE]
+    # posts = Post.objects.filter(group=group).order_by('-pub_date')
+    # [:POSTS_PER_PAGE]
     posts = Post.objects.all()[:POSTS_PER_PAGE]
     context = {
         'group': group,
